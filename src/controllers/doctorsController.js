dce44b3fea26de40079a0eeda264239ef2f00c18
Guilherme Sanches <guitdsanches@gmail.com>
2023-11-11 16:44:08 -0300

    feat(doctors): adding post and put method

--- src/controllers/doctorsController.js
+++ src/controllers/doctorsController.js
@@ -6,7 +6,45 @@ class DoctorsController {
             const doctorsList = await doctors.find();
             res.status(200).json(doctorsList);
         } catch (error) {
-            console.error('erro na requisicao: ', error);
+            res.status(400).json({ message: error });
+        }
+    }
+
+    static async getDoctorById(req, res, next) {
+        try {
+            const id = req.params.id;
+            const searchedDoctor = await doctors.findById(id);
+            if (searchedDoctor !== null) {
+                res.status(200).json(searchedDoctor);
+            } else {
+                res.status(404).json({ message: 'Doctor not found!' });
+            }
+        } catch (error) {
+            res.status(400).json({ message: error });
+        }
+    }
+
+    static async saveDoctor(req, res, next) {
+        try {
+            const doctorData = req.body;
+            const newDoctor = await doctors.create(doctorData);
+            res.status(201).json({ message: 'Doctor saved successfully', doctor: newDoctor });
+        } catch (error) {
+            res.status(400).json({ message: error });
+        }
+    }
+
+    static async updateDoctor(req, res, next) {
+        try {
+            const id = req.params.id;
+            if (await doctors.findByIdAndUpdate(id, req.body) !== null) {
+                const doctorUpdated = await doctors.findById(id);
+                res.status(200).json(doctorUpdated);
+            } else {
+                res.status(404).json({ message: 'Doctor not found!' });
+            }
+        } catch (error) {
+            res.status(400).json({ message: error });
         }
     }
 }
--- src/routes/doctorsRoutes.js
+++ src/routes/doctorsRoutes.js
@@ -4,5 +4,8 @@ import DoctorsController from '../controllers/doctorsController.js';
 const routes = express.Router();
 
 routes.get("/doctors", DoctorsController.getDoctors);
+routes.get("/doctors/:id", DoctorsController.getDoctorById);
+routes.post("/doctors", DoctorsController.saveDoctor);
+routes.put("/doctors/:id", DoctorsController.updateDoctor);
 
 export default routes;
